a4da5c8ec990f096016390b15096aab0aa26004c
khushibansal554 <khushibansal554@gmail.com>
2026-06-18 23:49:59 +0530

    add DAY_10

--- DAY_10/Q39.C
+++ DAY_10/Q39.C
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int rows = 5;  // total number of rows in the pyramid
+    int rows = 5; 
 
-    // Outer loop: controls each row
     for(int i = 1; i <= rows; i++) {
         
-        // First inner loop: print spaces before stars
-        // Spaces increase as we go down each row
-        for(int j = 1; j < i; j++) {
+        // First inner loop: print spaces before numbers
+        // Spaces decrease as we go down each row
+        for(int j = 1; j <= rows - i; j++) {
             printf(" ");
         }
 
-        // Second inner loop: print stars
-        // Stars decrease by 2 each row
-        for(int k = 1; k <= (2 * (rows - i) + 1); k++) {
-            printf("*");
+        // Second inner loop: print ascending numbers (1 to i)
+        for(int k = 1; k <= i; k++) {
+            printf("%d", k);
+        }
+
+        // Third inner loop: print descending numbers (i-1 down to 1)
+        for(int k = i - 1; k >= 1; k--) {
+            printf("%d", k);
         }
 
         printf("\n");
